bba2052b1f86a7e848248dee1559c3e5f5b88375
Maycon J Pereira <mayconjovan@gmail.com>
2021-03-27 20:33:43 -0300

    Correct wrong data

--- pre_processamendo_credit_data.py
+++ pre_processamendo_credit_data.py
@@ -1,2 +1,9 @@
 import pandas as pd
 base = pd.read_csv('credit_data.csv')
+base.describe()
+base.loc[base['age'] < 0]
+# apagar a coluna
+base.drop('age', 1, inplace=True)
+# apagar somente os registros com problema
+base.drop(base[base.age < 0].index, inplace=True)
+# preencher os valores manualmente
